b10bd41fdb4301c210a6d920cace893fc7bb2a6a
Mohit Kumar Gola <mohitprajapati2444@gmail.com>
2026-07-10 00:58:04 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (61.40%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -0,0 +1,62 @@
+class DisjointSet{
+    vector<int>rank,parent;
+     public :DisjointSet(int n){
+        rank.resize(n+1,0);
+        parent.resize(n+1);
+
+        for(int i=0;i<=n;i++ ) parent[i]=i;
+     }
+
+    int  findUparent(int node){
+        if(parent[node]==node) return node;
+        else  return  parent[node]=findUparent(parent[node]);
+    }
+    void  UnionByRank(int  u,int  v){
+        int ulp_v=findUparent(v);
+        int ulp_u=findUparent(u);
+
+        if(parent[u]==parent[v]) return;
+        else{
+            if(rank[ulp_v]<rank[ulp_u]){
+
+                parent[ulp_v]=ulp_u;
+            }
+            else if(rank[ulp_u]<rank[ulp_v]){
+                parent[ulp_u]=ulp_v;
+            }
+            else{
+                parent[ulp_v]=ulp_u;
+                rank[ulp_u]++;
+            }
+        }
+    }
+
+    int connected(){
+
+        int ans=0;
+        for(int  i=0;i<parent.size();i++){
+            if(parent[i]==i) ans++;
+        }
+        return ans;
+    }
+
+    
+};
+
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n=isConnected.size();
+        DisjointSet ds(n);
+        for(int  i=0;i<isConnected.size();i++){
+            for(int  j=0;j<isConnected[i].size();j++){
+                if(isConnected[i][j]==1){
+                    ds.UnionByRank(i,j);
+                    
+                }
+            }
+            
+         }
+         return ds.connected()-1;
+    }
+};
